79931c03b52e7e8f71b46a7b1102addf70413b8a
Joseph Joslin <jjoslin07@gmail.com>
2021-08-18 18:17:09 -0700

    P3C2

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+  name: "MenuItem",
+  props: {
+    addToShoppingCart: {
+      type: Function,
+      required: true,
+    },
+    image: {
+      type: Object,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      defaut: 1,
+    },
+  },
+  data() {
+    return {
+      onSale: false,
+    };
+  },
+  computed: {
+    generatedPrice() {
+      if (this.onSale) {
+        return (this.price * 0.9).toFixed(2);
+      } else {
+        return this.price;
+      }
+    },
+  },
+  beforeMount() {
+    const today = new Date().getDate();
+
+    if (today % 2 === 0) {
+      this.onSale = true;
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="menu-item">
+    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
+    <div>
+      <h3>{{ name }}</h3>
+      <p>Price: {{ generatedPrice }} <span v-if="onSale">(10% off!)</span></p>
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <div>
+        <label for="add-item-quantity">Quantity: {{ quantity }}</label>
+        <input v-model.number="quantity" id="add-item-quantity" type="number" />
+        <button @click="addToShoppingCart(quantity)">
+          Add to Shopping Cart
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.menu-item {
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+  margin-bottom: 30px;
+
+  &__image {
+    max-width: 300px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="app" class="app">
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
+      and french pastries! Give you morning a warm start or treat yourself in
+      the middle of the day. Our butter is imported from local farmers in
+      France. Once you take your first bite, you will see why everyone can't get
+      enough!
+    </p>
+
+    <section class="menu">
+      <h2>Menu</h2>
+      <MenuItem
+        v-for="item in simpleMenu"
+        :addToShoppingCart="addToShoppingCart"
+        :name="item.name"
+        :image="item.image"
+        :price="item.price"
+        :quantity="item.quantity"
+        :inStock="item.inStock"
+        :key="item.name"
+      />
+    </section>
+
+    <aside class="shopping-cart">
+      <h2>Shopping Cart: {{ shoppingCart }} items</h2>
+    </aside>
+
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import MenuItem from "../components/MenuItem";
+
+export default {
+  name: "Home",
+  components: {
+    MenuItem,
+  },
+  data() {
+    return {
+      restaurantName: "Cafe with A Vue",
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: "Crossiant",
+          image: {
+            source: "/images/crossiant.jpg",
+            alt: "A crossiant",
+          },
+          inStock: true,
+          quantity: 1,
+          price: 2.99,
+        },
+        {
+          name: "French Baguette",
+          image: {
+            source: "/images/french-baguette.jpeg",
+            alt: "Four French Baguettes",
+          },
+          inStock: true,
+          quantity: 1,
+          price: 3.99,
+        },
+        {
+          name: "Éclair",
+          image: {
+            source: "/images/eclair.jpg",
+            alt: "Chocolate Éclair",
+          },
+          inStock: false,
+          quantity: 1,
+          price: 4.99,
+        },
+      ],
+    };
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear();
+
+      return `Copyright ${this.restaurantName} ${currentYear}`;
+    },
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.description {
+  max-width: 960px;
+  font-size: 1.2rem;
+  margin: 0 auto;
+}
+
+.footer {
+  text-align: center;
+  font-style: italic;
+}
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.shopping-cart {
+  position: absolute;
+  right: 30px;
+  top: 0;
+}
+</style>
